d9f67bb33dbe8422cfe9f552858a01cbaadb0cea
sytone <github@sytone.com>
2015-09-24 17:55:54 -0700

    Addition of shell for TaskListStorage plugin.

---
+++ TaskListStorage/SampleTaskListStorage/SampleTaskListStorageCore/SampleTaskListStorageCore.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using TDLPluginHelpers;
+
+namespace SampleTaskListStorage
+{
+    public class SampleTaskListStorageCore
+    {
+        public bool Export(TDLTaskList srcTasks, string sDestFilePath, bool bSilent, TDLPreferences prefs, string sKey)
+        {
+            int nVal = prefs.GetProfileInt("bob", "dave", 20);
+            int nVal2 = prefs.GetProfileInt("bob", "phil", 20);
+
+            // add some dummy values to prefs
+            prefs.WriteProfileInt("bob", "dave", 10);
+
+            TDLTask task = srcTasks.GetFirstTask();
+
+            String sTitle = task.GetTitle();
+
+
+            return true;
+        }
+    }
+}
